e0d1d375327ca726117703e3e6559f8d0db64b0d
mromina00 <mromina03@gmail.com>
2023-12-02 22:42:10 -0300

    agregue sección de login

--- js/main.js
+++ js/main.js
@@ -158,3 +158,17 @@ const add_btn_scrolltop = () => {
         btn_scrolltop.classList.add("btn-scrolltop-on");
     }
 }
+
+// Botón de Inicio de Sesión
+
+const wrapper = document.querySelector('.wrapper');
+const btnPopup = document.querySelector('.btnLogin-popup');
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
